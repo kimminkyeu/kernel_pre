78a4fba82102f2c6b12089288045a1147b5a8db5
kimminkyeu <minkyeki@student.42seoul.kr>
2024-07-10 15:48:45 +0900

    :heavy_plus_sign: chore: initialize mvc project
    - build: gradle groovy
    - spring boot
    - spring web mvc
    - lombok

---
+++ src/main/java/sample/pre/PreApplication.java
@@ -0,0 +1,12 @@
+package sample.pre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreApplication.class, args);
+	}
+}
---
+++ src/test/java/sample/pre/PreApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.pre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
