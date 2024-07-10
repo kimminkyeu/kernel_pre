eeca4dc8b8eff1ee34b9f217f24fa761e26984c1
kimminkyeu <minkyeki@student.42seoul.kr>
2024-07-11 00:20:46 +0900

    :sparkles: feat(Ch 02. REST API): CRUD 어노테이션 예제 학습
    GET, POST, PUT, DELETE 요청을 받는 법을 강의를 통해 학습한다.

---
+++ src/main/java/sample/pre/UserSearchQueryParam.java
@@ -0,0 +1,14 @@
+package sample.pre;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+ @Data @Getter @Setter
+ @NoArgsConstructor @AllArgsConstructor
+public class UserSearchQueryParam {
+	private Integer age;
+	private Integer country;
+}
