3f2930c4fc5b473cefcd675c78facb3ff6a71d81
kimminkyeu <minkyeki@student.42seoul.kr>
2024-07-14 17:18:01 +0900

    :recycle: chore: 미사용 import 제거

--- src/main/java/sample/pre/article/mapper/ArticleMapper.java
+++ src/main/java/sample/pre/article/mapper/ArticleMapper.java
@@ -10,9 +10,9 @@ import sample.pre.board.entity.BoardEntity;
 /**
  * NOTE: ArticleMapper 엔티티는 BoardEntity를 받아야 한다. 그래야 OneToMany 관계 형성이 된다.
  *       Mapper에서 service를 주입받을 수도 있지만, 객체 관계 상 파라미터로 받는게 맞다고 판단된다.
- *       https://stackoverflow.com/questions/57010906/in-service-oriented-architecture-is-it-a-good-practise-that-a-mapper-calls-a-se
  *       일단 좀 더 고민이 필요하다.
  *       이 부분 때문에 강의에서는 FK로 안했나 보다...
+ * @Refernce <a href="https://stackoverflow.com/questions/57010906/in-service-oriented-architecture-is-it-a-good-practise-that-a-mapper-calls-a-se">...</a>
  */
 //public class ArticleMapper implements EntityDtoConvertable<ArticleEntity, ArticleCreateRequestDto> {
 public class ArticleMapper {
