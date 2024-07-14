1b0f7f40aab86fb035be938a267ca8d241dd924b
kimminkyeu <minkyeki@student.42seoul.kr>
2024-07-14 16:52:42 +0900

    :sparkles: feat: Pagination - Article 페이지 조회 기능 구현

---
+++ src/main/java/sample/pre/common/PaginatedListDto.java
@@ -0,0 +1,20 @@
+package sample.pre.common;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaginatedListDto<T> {
+	private List<T> contentList;
+	private Pagination pagination;
+}
